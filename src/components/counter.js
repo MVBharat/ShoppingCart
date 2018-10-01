adfbaa2631e4b9d1481b295df390fcf58454cae7
MVBharat <manebharat85@gmail.com>
2018-10-01 17:44:12 +0530

    Add conditional rendering

--- src/components/counter.js
+++ src/components/counter.js
@@ -4,6 +4,7 @@ class Counter extends Component{
   state = {
     count: 0,
     // imageUrl: 'https://picsum.photos/200'
+    tags: []
   };
 
   styles = {
@@ -11,12 +12,19 @@ class Counter extends Component{
     fontWeight: "bold"
   }
 
+  renderTags(){
+    if (this.state.tags.length === 0) return <p>There are no values! </p>;
+
+    return   <ul> {this.state.tags.map(tag  => <li key ={tag }>{tag}  </li> )} </ul> ;
+  }
   render(){
 
     return (
           <React.Fragment>
             <span style ={this.styles} className={this.getBadgeClasses()} > {this.formatCount()} </span>
             <button className="btn btn-secondary btn-sm">Increment </button>
+            {this.state.tags.lenght === 0 && "Please create a new tag!"}
+            {this.renderTags()}
           </React.Fragment>
         );
   }
