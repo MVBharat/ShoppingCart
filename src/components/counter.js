0217c91b3ba604e936919baf200d545259b70a21
MVBharat <manebharat85@gmail.com>
2018-10-01 17:24:05 +0530

    increment values color changes dynamically

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 
 class Counter extends Component{
   state = {
-    count: 0
+    count: 0,
+    // imageUrl: 'https://picsum.photos/200'
   };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold"
+  }
+
   render(){
+
     return (
           <React.Fragment>
-            <span> {this.formatCount()} </span>
-            <button>Increment </button>
+            <span style ={this.styles} className={this.getBadgeClasses()} > {this.formatCount()} </span>
+            <button className="btn btn-secondary btn-sm">Increment </button>
           </React.Fragment>
         );
+  }
+
+  getBadgeClasses(){
+    let classes = "badge m-2 p-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount(){
