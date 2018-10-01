048a7852cd6a8920fbfc0af656c9216fc41a0e20
MVBharat <manebharat85@gmail.com>
2018-10-01 18:22:06 +0530

    Add increment button to add Cart

--- src/components/counter.js
+++ src/components/counter.js
@@ -3,28 +3,25 @@ import React, { Component } from 'react';
 class Counter extends Component{
   state = {
     count: 0,
-    // imageUrl: 'https://picsum.photos/200'
-    tags: []
   };
 
+
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  }
   styles = {
     fontSize: 10,
     fontWeight: "bold"
   }
 
-  renderTags(){
-    if (this.state.tags.length === 0) return <p>There are no values! </p>;
-
-    return   <ul> {this.state.tags.map(tag  => <li key ={tag }>{tag}  </li> )} </ul> ;
-  }
   render(){
 
     return (
           <React.Fragment>
             <span style ={this.styles} className={this.getBadgeClasses()} > {this.formatCount()} </span>
-            <button className="btn btn-secondary btn-sm">Increment </button>
-            {this.state.tags.lenght === 0 && "Please create a new tag!"}
-            {this.renderTags()}
+            <button onClick={() => this.handleIncrement({id: 1})}
+             className="btn btn-secondary btn-sm">Increment </button>
           </React.Fragment>
         );
   }
