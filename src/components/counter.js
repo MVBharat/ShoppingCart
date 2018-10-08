b035b2ae5660c7505796d1d02d12db48ccc6367d
MVBharat <manebharat85@gmail.com>
2018-10-08 14:33:42 +0530

    Add cart values increment option

--- src/components/counter.js
+++ src/components/counter.js
@@ -10,14 +10,20 @@ class Counter extends Component{
   render(){
 
     return (
-          <React.Fragment>
-            <span style ={this.styles} className={this.getBadgeClasses()} > {this.formatCount()} </span>
-            <button onClick={() => this.props.onIncrement(this.props.counter)}
-             className="btn btn-secondary btn-sm m-2"> Increment </button>
-            <button onClick ={() => this.props.onDelete(this.props.counter.id)}
-             className="btn btn-danger btn-sm m-1"> Delete </button>
+          <div>
+            <span className={this.getBadgeClasses()} > {this.formatCount()} </span>
+            <button
+              onClick={() => this.props.onIncrement(this.props.counter)}
+              className="btn btn-secondary btn-sm m-2">
+              Increment
+            </button>
+            <button
+              onClick ={() => this.props.onDelete(this.props.counter.id)}
+              className="btn btn-danger btn-sm m-1">
+              Delete
+            </button>
             <br/>
-          </React.Fragment>
+          </div>
         );
   }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 
-
-class NavBar extends Component{
-
-  render(){
-
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
-      </nav>
-    )
-
-  }
-}
+const NavBar = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar
+        <span className="badge badge-pill badge-secondary m-2">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
